24ad73d5376504a55a91aa242e6d862a7135f3d8
Hakan Ayaz <43383132+hakanayaz@users.noreply.github.com>
2021-01-09 16:27:25 -0700

    partway through updating training script

--- train_recognizer.py
+++ train_recognizer.py
@@ -4,50 +4,81 @@ import os
 import glob
 from PIL import Image
 
+
+# Define paths for DNN
+base_dir = os.path.dirname(__file__)
+prototxt_path = os.path.join(base_dir + '/model_data/deploy.prototxt')
+caffemodel_path = os.path.join(base_dir + '/model_data/weights.caffemodel')
+# Read the model with DNN algorithm
+model = cv2.dnn.readNetFromCaffe(prototxt_path, caffemodel_path)
+
 # Creating the dataset and labeled the Training and Test data
 # Defining the directory for the Training and Test datasets
 TRAININGSET_DIRECTORY = 'TrainingDataset'
-TESTSET_DIRECTORY = 'TestDataset'
 
 
 assert (os.path.exists(TRAININGSET_DIRECTORY))
+
 Training_Set_Names = glob.glob(os.path.join(TRAININGSET_DIRECTORY, "*.jpg"))
+Training_Set_Names.extend(glob.glob('*.JPG'))
+#Training_Set_Names = os.listdir(base_dir+'/TrainingDataset/')
 assert (len(Training_Set_Names) > 0)
-assert (os.path.exists(TESTSET_DIRECTORY))
-Test_Set_Names = glob.glob(os.path.join(TESTSET_DIRECTORY, "*.jpg"))
-assert (len(Test_Set_Names) > 0)
 
 Training_Set_Names.sort()
-Test_Set_Names.sort()
 font = cv2.FONT_HERSHEY_SIMPLEX
 
 
-def getFacesAndLabels(paths, detector):
+def getFacesAndLabels(paths):
     faceSamples = []
     ids = []
+    count = 0
     for imagePath in paths:
-        PIL_img = Image.open(imagePath).convert('L') # convert it to grayscale
-        img_numpy = np.array(PIL_img,'uint8')
+        # PIL_img = Image.open(imagePath).convert('L') # convert it to grayscale
+        # img_numpy = np.array(PIL_img,'uint8')
+
+        img_numpy = cv2.imread(imagePath)
+        gray = cv2.cvtColor(img_numpy, cv2.COLOR_BGR2GRAY)
         id = int(os.path.split(imagePath)[-1].split(".")[1])
 
-        faces = detector.detectMultiScale(img_numpy)
-        for (x, y, w, h) in faces:
-            faceSamples.append(img_numpy[y:y+h,x:x+w])
-            ids.append(id)
+        # DNN
+        (h, w) = gray.shape[:2]
+        blob = cv2.dnn.blobFromImage(cv2.resize(gray, (300, 300)), 1.0,  # ***
+                                     (300, 300), (104.0, 177.0, 123.0))
+        model.setInput(blob)
+        detections = model.forward()
+
+        # faces = detector.detectMultiScale(img_numpy)
+        # for (x, y, w, h) in faces:
+        #     faceSamples.append(img_numpy[y:y+h,x:x+w])
+        #     ids.append(id)
+
+        # Identify each face
+        for i in range(0, detections.shape[2]):
+            box = detections[0, 0, i, 3:7] * np.array([w, h, w, h])
+            (startX, startY, endX, endY) = box.astype("int")
+
+            confidence = detections[0, 0, i, 2]
+
+            # If confidence > 0.5, append it to the face samples
+            if (confidence > 0.5):
+                count += 1
+                frame = gray[startY:endY, startX:endX]  # ***
+                faceSamples.append(frame)
+                ids.append(id)
+
     return faceSamples, ids
 
 
 def main():
-
-    detector = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+    # detector = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
     recognizer = cv2.face.LBPHFaceRecognizer_create()
     # recognizer.read('trainer/trainer.yml')
 
     print("\n Training faces ...")
-    faces, ids = getFacesAndLabels(Training_Set_Names, detector)
+    faces, ids = getFacesAndLabels(Training_Set_Names)
     recognizer.train(faces, np.array(ids))
     # Save the model into trainer/Trained Model.yml
-    recognizer.write('trainer/Trained_Model.yml')
+    recognizer.write('trainer/Trained_Model_w_DNN.yml')
     # Print the number of faces trained and end program
     print("\n Faces trained.")
 
